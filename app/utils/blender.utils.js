dba1657d33ecfa355880871d5e767742c07e2d6c
jimyluke <myzsoftdev1@gmail.com>
2022-05-31 21:23:47 +0300

    Change command for blender rendering

--- app/utils/blender.utils.js
+++ app/utils/blender.utils.js
@@ -69,7 +69,7 @@ exports.renderTokenFromBlender = async (
   );
 
   const { stderr } = await exec(
-    `blender 1> nul -b -noaudio --addons ${addonName} --python-expr "import bpy;bpy.ops.crypto_quest_test.render_from_json(jsonPath= '${blenderOutputFolderPathAbsolute}${tokenId}.json', outDirPath = '${blenderOutputFolderPathAbsolute}')"`
+    `blender 1> nul -b -noaudio --addons CryptoQuest --python-expr "import bpy;bpy.ops.crypto_quest.render_from_json(jsonPath= '${blenderOutputFolderPathAbsolute}${tokenId}.json', outDirPath = '${blenderOutputFolderPathAbsolute}')"`
   );
 
   if (stderr) {
